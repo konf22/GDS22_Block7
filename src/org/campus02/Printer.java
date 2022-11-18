f99f190811f05965a823b101f631ab06385e9ff0
dkandlhofer <daniel.kandlhofer@lv.campus02.at>
2022-11-18 18:55:18 +0100

    firt recursion - Printer

---
+++ src/org/campus02/Printer.java
@@ -0,0 +1,41 @@
+package org.campus02;
+
+public class Printer {
+
+    public static void main(String[] args) {
+        //printFromNToZero(5);
+        //printFromNToZeroRecursive(5);
+        printFromZeroToNRecursive(5);
+    }
+
+    // Methodendeklaration
+    public static void printFromNToZero(int n) {
+        while (n >= 0) {
+            System.out.println("n = " + n);
+            n--;
+            // n = n - 1;
+        }
+    }
+
+    public static void printFromNToZeroRecursive(int n) {
+        // abbruchsbestimmung
+        if (n < 0) {
+            return;
+        }
+        System.out.println("n = " + n);
+        // rufe mich selbst nochmal auf
+        printFromNToZeroRecursive(n - 1); //n updaten (1 verringern)
+    }
+
+    public static void printFromZeroToNRecursive(int n) {
+        // abbruchsbestimmung
+        if (n < 0) {
+            return;
+        }
+        // rufe mich selbst nochmal auf
+        printFromZeroToNRecursive(n - 1); //n updaten (1 verringern)
+
+        // schreibe erst auf die Konsole wenn die Abbruchsbestimmung zutrifft
+        System.out.println("n = " + n);
+    }
+}
